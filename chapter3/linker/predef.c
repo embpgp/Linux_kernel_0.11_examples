e6fa399dd91219933ab7788af298712027e57612
embpgp <522943492@qq.com>
2016-11-14 12:45:23 +0800

    Print the symbol predefinded by linker

---
+++ chapter3/linker/predef.c
@@ -0,0 +1,15 @@
+/*
+ Print the symbol predefined by linker
+ */
+
+extern int end, etext, edata;
+extern int _end, _etext, _edata;
+
+int main(void)
+{
+	printf("&etext = %p, &edata = %p, &end = %p\n", \
+			&etext, &edata, &end);
+	printf("&_etext = %p, &_edata = %p, &_end = %p\n", \
+			&_etext, &_edata, &_end);
+	return 0;	
+}
